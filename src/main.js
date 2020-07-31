d18573b93549ca75ebe993b1759a2313ccc368d2
俞扬军 <2273493680@qq.com>
2020-07-31 17:21:05 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//动态加载rem基准值
+import 'amfe-flexible'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
